b6ffb1b1431111a8323dbba3bc120d4bf8d5f541
Yuvaraj878 <ai.yuvaraj21@gmail.com>
2024-03-15 21:30:48 +0530

    updated the profile page

--- crime/crime/urls.py
+++ crime/crime/urls.py
@@ -21,4 +21,5 @@ from crimeapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',views.login,name='login'),
+    path('profile/',views.profile,name='profile')
 ]
--- crime/crimeapp/views.py
+++ crime/crimeapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 def login(request):
     context={}
     return render(request,'login.html',context)
+
+def profile(request):
+    context={}
+    return render(request,'profile.html',context)
