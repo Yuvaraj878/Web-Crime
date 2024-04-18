837e608b16586b98dbfa55f589042fe00ab4fda4
Yuvaraj878 <ai.yuvaraj21@gmail.com>
2024-04-18 14:35:52 +0530

    Add User model to Public class in models.py and update emergency.html with AJAX calls for SOS signal and threat details

--- crime/crimeapp/models.py
+++ crime/crimeapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 class Public(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
     gender = models.CharField(max_length=10, choices=(('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')))
